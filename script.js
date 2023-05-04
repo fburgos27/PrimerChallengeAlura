9a6b4171a41463aa3c5a9576a128991716e50708
Fabian <fburgos27@gmail.com>
2023-05-03 23:05:31 -0400

    agregando las funciones de Javascript, cambiando letras en CSS y combiando clases e id en html a inglés

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
-function borrarAreaTexto() {
-    document.getElementById("areaEncriptado").style.display = "none";
-  }
+const textArea = document.querySelector(".encrypt-text");
+const msg = document.querySelector(".encrypted-area");
 
-function encriptar(mensaje) {
-    let mensaje = mensaje;
-
-    for (i=0, i < length(mensaje), i=i+1) {
-        if (mensaje[i] == "e") {
-            mensaje[i] == "enter";
-        }
-        console.log(mensaje);
-    }
-    
+function encryptButton() {
+    let encryptedText = encrypt(textArea.value);
+    msg.value = encryptedText;
+    textArea.value = "";
+    msg.style.backgroundImage = "none";
 }
 
-function desencriptar() {
-    
+function encrypt(text) {
+    text = text.toLowerCase()
+    let normalText = text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    text = normalText;
+    let encryptText = text.replace(/e/g, 'enter');
+    encryptText = encryptText.replace(/i/g, 'imes');
+    encryptText = encryptText.replace(/a/g, 'ai');
+    encryptText = encryptText.replace(/o/g, 'ober');
+    encryptText = encryptText.replace(/u/g, 'ufat');
+    return encryptText;
 }
+
+function decryptButton() {
+    let decryptedText = decrypt(textArea.value);
+    msg.value = decryptedText;
+    textArea.value = "";
+    msg.style.backgroundImage = "none";
+}
+
+function decrypt(text) {
+    const normalText = text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    text = normalText;
+    text = text.toLowerCase()    
+    let decryptText = text.replace(/enter/g, 'e');
+    decryptText = decryptText.replace(/imes/g, 'i');
+    decryptText = decryptText.replace(/ai/g, 'a');
+    decryptText = decryptText.replace(/ober/g, 'o');
+    decryptText = decryptText.replace(/ufat/g, 'u');
+    return decryptText;
+}
+
+function copyButton() {
+    let text = document.getElementById("e-area").value;
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        alert("Texto copiado al portapapeles");
+    })
+    .catch((error) => {
+        alert("Error al copiar el texto: ", error);
+    });
+
+}
+
